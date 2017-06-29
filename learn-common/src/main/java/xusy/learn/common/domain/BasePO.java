824fdd1d0cfe4435ee91c3c77ad59aae6f319a42
siyong.xu <liuxingxsy@163.com>
2017-06-29 18:04:49 +0800

    Initial commit

---
+++ learn-common/src/main/java/xusy/learn/common/domain/BasePO.java
@@ -0,0 +1,97 @@
+package xusy.learn.common.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 基础持久化对象，含各持久化对象公用属性
+ * Created by siyong.xu on 2017-06-29.
+ */
+public class BasePO implements Serializable {
+    private static final long serialVersionUID = -1806855615293661692L;
+    /**
+     * 主键id
+     */
+    private Long id;
+    /**
+     * 创建者
+     */
+    private String  creator;
+    /**
+     * 创建时间
+     */
+    private Date gmtCreated;
+    /**
+     * 修改者
+     */
+    private String modifier;
+    /**
+     * 修改时间
+     */
+    private Date gmtModified;
+    /**
+     * 是否删除
+     */
+    private String isDelete;
+    /**
+     * 版本号
+     */
+    private String version;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public Date getGmtCreated() {
+        return gmtCreated;
+    }
+
+    public void setGmtCreated(Date gmtCreated) {
+        this.gmtCreated = gmtCreated;
+    }
+
+    public String getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(String modifier) {
+        this.modifier = modifier;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public String getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(String isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+}
---
+++ learn-common/src/main/java/xusy/learn/common/exceptions/BizException.java
@@ -0,0 +1,8 @@
+package xusy.learn.common.exceptions;
+
+/**
+ * Created by siyong.xu on 2017-06-29.
+ */
+public class BizException extends RuntimeException {
+    private static final long serialVersionUID = 2461955237602618850L;
+}
